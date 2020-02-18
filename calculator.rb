8c281f49c0f62e9ca87ff3e95323ac284f1930e6
Dakota Willingham <kota@vaga.tk>
2020-02-18 19:57:21 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,5 +4,5 @@ second_number = 5
 
 sum = (first_number + second_number)
 difference = (first_number - second_number)
-product = (first_number * y)
-quotient = (first_number)
+product = (first_number * second_number)
+quotient = (first_number / second_number)
